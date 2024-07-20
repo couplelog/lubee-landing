cc080183fd469ec33ae0afe8c04adaa77c209545
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-20 16:29:06 +0900

    Update App.tsx

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -3,7 +3,6 @@ import FuncThree from "./FuncThree";
 import FuncTwo from "./FuncTwo";
 import Yellow from "./Yellow";
 import Footer from "./Footer";
-import { useState } from "react";
 import Intro from "./Intro";
 
 function App() {
